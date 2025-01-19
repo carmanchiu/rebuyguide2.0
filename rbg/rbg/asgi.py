ca2f5f2b4a1ed2101c95d6f1d0593c9af9322fb2
chiucarman <omgitzcarman@gmail.com>
2025-01-19 16:10:34 -0600

    created new project

--- rbg/rbg/asgi.py
+++ rbg/rbg/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for rebuyguide project.
+ASGI config for rbg project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rebuyguide.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rbg.settings')
 
 application = get_asgi_application()
--- rbg/rbg/urls.py
+++ rbg/rbg/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for rebuyguide project.
+URL configuration for rbg project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -20,5 +20,3 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
-
-# test
