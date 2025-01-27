ca3642fd9e9eb1179ea794989e2c4dee027cc688
chiucarman <omgitzcarman@gmail.com>
2025-01-27 16:13:48 -0600

    hooking up the URL mapper

--- rbg/rbg/urls.py
+++ rbg/rbg/urls.py
@@ -17,11 +17,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls import include
+from django.views.generic import RedirectView
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
-
-urlpatterns += [
     path('catalog/', include('catalog.urls')),
-]
+    path('', RedirectView.as_view(url='catalog/')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
